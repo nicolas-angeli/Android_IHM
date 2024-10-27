d7b6be0dc92986261629e7489f7f649b41cd912b
Noel Flantier <moi>
2024-10-27 16:07:25 +0100

    Ajour de la classe Salle Et classe list-salle dans le main activity

--- Firebase/app/src/main/java/github/yvesbenabou/firebase/MainActivity.java
+++ Firebase/app/src/main/java/github/yvesbenabou/firebase/MainActivity.java
@@ -1,65 +1,90 @@
 package github.yvesbenabou.firebase;
 
-import android.content.Context;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity implements Database_Out {
-  private final String floors = "étages";
-  TextView txt;
-  String TAG = "MainActivity";
+import java.util.HashMap;
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+import github.yvesbenabou.firebase.libs.*;
 
-    txt = (TextView) findViewById(R.id.text);
+public class MainActivity extends AppCompatActivity {
+    TextView txt;
 
-    TakeRoomButton trb = findViewById(R.id.trb);
-    trb.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View view) {
-        // On Click
-        // Write data to Firebase Database
-        trb.take_room("1407");
-      }
-    });
+    String TAG = "MainActivity";
+    String fbKey="salles";
+    Integer count;
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        count = 0;
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
 
-    final DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference();  // and if you explicit the project name with full URL, you get an exception with forbidden characters!
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-    // Read data from Firebase Database
-    databaseRef.child(floors).addValueEventListener(new ValueEventListener() {
-      @Override public void onDataChange(DataSnapshot dataSnapshot) {
-        txt.setText(dataSnapshot.getValue().toString());
-      }
-      @Override
-      public void onCancelled(DatabaseError databaseError) {
-        // Getting Post failed, log a message
-        Log.w(TAG, "onCancelled", databaseError.toException());
-      }
-    });
+        txt = (TextView) findViewById(R.id.text);
 
-  }
+    /*
+	the result depends on your android configuration:
+	this works up to android 33 and firebase database 19.2.1 (see ../dependencies.gradle)
+    */
+        final DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();  // and if you explicit the project name with full URL, you get an exception with forbidden characters!
+    /* 
+	some students had issues with the syntax above, they had to explicit the name of the project again
+    */
+        //final DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference("https://projet1-38f7d.firebaseio.com"); // explicitly adding the project url avoids hoping for project to be implicitly hosted on server us-central1
+        rootRef.child(fbKey).child("1007").setValue(0);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                // On Click
+                // Write data to Firebase Database
+                rootRef.child(fbKey).setValue("Yes! We're sending message " + count.toString() +" from App to Firebase.");
+                count++;
+            }
+        });
 
-  @Override
-  public void update() {
-    //cette fonction sera appelée toutes les 15 mins pour resynchroniser avec ADE
+        // Read data from Firebase Database
+        rootRef.child(fbKey).addValueEventListener(new ValueEventListener() {
+            @Override public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.i(TAG,
+                        dataSnapshot.getValue().toString());  //reads back data with key fbKey
+                txt.setText(dataSnapshot.getValue().toString());
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "onCancelled", databaseError.toException());
+            }
+        });
 
-  }
+    }
+
+    public static class Liste_Salles {
+
+        HashMap<String, Salle> List;
+
+        public Liste_Salles() {
+            List = new HashMap<>();
+        }
+
+        public Salle getSalle(String num) {
+            return List.get(num);
+        }
+        public void setSalle(Salle newSalle) {
+            List.put(newSalle.getNum(), newSalle);
+        }
+    }
+
 }
