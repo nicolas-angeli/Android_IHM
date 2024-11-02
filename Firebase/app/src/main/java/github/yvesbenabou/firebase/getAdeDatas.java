4656033e8d5090b1fb229f06e1d6c3b65fbd01b3
Noel Flantier <moi>
2024-11-02 15:26:06 +0100

    Essais de récupération de datas

--- Firebase/app/src/main/java/github/yvesbenabou/firebase/getAdeDatas.java
+++ Firebase/app/src/main/java/github/yvesbenabou/firebase/getAdeDatas.java
@@ -11,9 +11,20 @@ public class getAdeDatas extends MainActivity{
         MainActivity.Liste_Salles adeDatas = new MainActivity.Liste_Salles();
 
 
-        // TODO à modifier lourdement (selenium)
-        String url = "https://planif.esiee.fr/direct/";
-        String htmlAdeDatas = WebPageSourceFetcher.getHtmlDatas(url);
+
+        // Exemple d'utilisation de la méthode
+        String url = "https://planif.esiee.fr/direct/"; // Remplacez par l'URL réelle
+        String username = "lecteur1"; // Remplacez par votre nom d'utilisateur
+        String password = ""; // Remplacez par votre mot de passe
+        String year = "2024-2025-ESIEE PARIS"; // Remplacez par l'année que vous souhaitez sélectionner
+
+
+        // Récupérer le code source de la page
+        String pageSource = WebPageSourceFetcher.fetchHtmlSource(url, username, password, year);
+
+        // Afficher le code source
+        System.out.println("Code source de la page :");
+        System.out.println(pageSource);
 
         for (Salle s : adeDatas.getList()) {
             System.out.println("Salle : " + s.getNum()+ " state : "+ s.getState() );
