0d845d06d43603c02bbb388c0f3c7f9aa6d5fc82
nicolas-angeli <125877992+nicolas-angeli@users.noreply.github.com>
2024-11-09 13:17:27 +0100

    Merging du dernier commit de Flo sur le mien (#8)
    * Fonction map
    
    * Boutton etage
    
    * Bouttons pour réserver la salle, afficher/cacher la bulle de réservation, changer les horaires
    
    * Images svg et zoom
    
    ---------
    
    Co-authored-by: EL5E31 <florian.bigotteau@edu.esiee.fr>

---
+++ Firebase/app/src/main/java/github/yvesbenabou/firebase/TakeRoomButton.java
@@ -0,0 +1,35 @@
+package github.yvesbenabou.firebase;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public final class TakeRoomButton extends FloatingActionButton implements Database_Input {
+  private final String floors = "étages";
+
+  public TakeRoomButton(@NonNull Context context) {
+    super(context);
+  }
+
+  public TakeRoomButton(@NonNull Context context, @Nullable AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public TakeRoomButton(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+
+  @Override
+  public void take_room(String room) {
+    FirebaseApp.initializeApp(this.getContext());
+    FirebaseDatabase.getInstance().getReference().child(floors).child(String.valueOf(room.charAt(1))).child(room).setValue(Status.OCCUPIED.ordinal());
+  }
+}
