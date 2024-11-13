16540ace1fb80c59d3020ac5147a512af624af25
nicolas-angeli <colas.angeli@gmail.com>
2024-11-13 11:57:40 +0100

    Ajout du nécessaire pour mettre à jour la base de donnée à partir d'ADE. (mettre un point d'arrêt l70 - CalendarFetcher)
    # Conflicts:
    #	Firebase/app/src/main/java/github/yvesbenabou/firebase/MainActivity.java
    #	Firebase/app/src/main/java/github/yvesbenabou/firebase/TakeRoomButton.java
    #	Firebase/app/src/main/res/drawable/dispo_salle.png
    #	Firebase/app/src/main/res/layout/content_main.xml

--- Firebase/app/src/main/java/github/yvesbenabou/firebase/MainActivity.java
+++ Firebase/app/src/main/java/github/yvesbenabou/firebase/MainActivity.java
@@ -40,6 +40,10 @@ import androidx.appcompat.app.AppCompatActivity;
 // Flo images svg zoom (reste déjà import)
 import android.view.ScaleGestureDetector;
 
+//Nico synchro de la base de données avec ADE
+import java.util.Calendar;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity implements Database_Out {
   private final String floors = "étages";
   TextView txt;
@@ -71,136 +75,142 @@ public class MainActivity extends AppCompatActivity implements Database_Out {
     setContentView(R.layout.activity_main);
     FirebaseDatabase.getInstance().setPersistenceEnabled(true);
 
-      //txt = (TextView) findViewById(R.id.text);
+    //txt = (TextView) findViewById(R.id.text);
 
-      DoorButton db = findViewById(R.id.doorbutton);
-      db.setup((ImageView) findViewById(R.id.takeroombubble),
-              findViewById(R.id.confirmroombutton),
-              findViewById(R.id.cancelbutton),
-              findViewById(R.id.modifybutton));
-      db.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-          // On Click
-          // Write data to Firebase Database
-          db.show();
-        }
-      });
+    DoorButton db = findViewById(R.id.doorbutton);
+    db.setup((ImageView) findViewById(R.id.takeroombubble),
+            findViewById(R.id.confirmroombutton),
+            findViewById(R.id.cancelbutton),
+            findViewById(R.id.modifybutton));
+    db.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        // On Click
+        // Write data to Firebase Database
+        db.show();
+      }
+    });
 
-      ConfirmRoomButton crb = findViewById(R.id.confirmroombutton);
-      crb.setup(db);  // setup the confirm button so that it can call the hide function of the DoorButton when clicked
-      crb.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-          // On Click
-          // Write data to Firebase Database
-          crb.take_room("2205");
-          crb.setColorNormal(crb.red);
-          crb.setColorRipple(crb.red);
-          crb.hide();
-        }
-      });
+    ConfirmRoomButton crb = findViewById(R.id.confirmroombutton);
+    crb.setup(db);  // setup the confirm button so that it can call the hide function of the DoorButton when clicked
+    crb.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        // On Click
+        // Write data to Firebase Database
+        crb.take_room("2205");
+        crb.setColorNormal(crb.red);
+        crb.setColorRipple(crb.red);
+        crb.hide();
+      }
+    });
 
-      final DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference();  // and if you explicit the project name with full URL, you get an exception with forbidden characters!
+    final DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference();  // and if you explicit the project name with full URL, you get an exception with forbidden characters!
 
-      CancelButton cb = findViewById(R.id.cancelbutton);
-      cb.setup(db);  // setup the cancel button so that it can call the hide function of the DoorButton when clicked
-      cb.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-          // On Click
-          cb.hide();
-        }
-      });
+    CancelButton cb = findViewById(R.id.cancelbutton);
+    cb.setup(db);  // setup the cancel button so that it can call the hide function of the DoorButton when clicked
+    cb.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        // On Click
+        cb.hide();
+      }
+    });
 
-      // Read data from Firebase Database
-      databaseRef.child(floors).addValueEventListener(new ValueEventListener() {
+    // Read data from Firebase Database
+    databaseRef.child(floors).addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange(DataSnapshot dataSnapshot) {
+        //txt.setText(dataSnapshot.getValue().toString());
 
-        @Override
-        public void onDataChange(DataSnapshot dataSnapshot) {
-          //txt.setText(dataSnapshot.getValue().toString());
+      }
 
-        }
-
-        @Override
-        public void onCancelled(DatabaseError databaseError) {
-          // Getting Post failed, log a message
-          Log.w(TAG, "onCancelled", databaseError.toException());
-        }
-      });
+      @Override
+      public void onCancelled(DatabaseError databaseError) {
+        // Getting Post failed, log a message
+        Log.w(TAG, "onCancelled", databaseError.toException());
+      }
+    });
 
 
-      //Flo boutton etage
-      backgroundImage = findViewById(R.id.backgroundImage);
-      Button slideButton = findViewById(R.id.slideButton);
+    //Flo boutton etage
+    backgroundImage = findViewById(R.id.backgroundImage);
+    Button slideButton = findViewById(R.id.slideButton);
 
-      // Initialiser l'affichage du texte du bouton avec le numéro d'image
-      slideButton.setText(String.valueOf(currentIndex));
+    // Initialiser l'affichage du texte du bouton avec le numéro d'image
+    slideButton.setText(String.valueOf(currentIndex));
 
-      slideButton.setOnTouchListener(new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-          switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-              initialY = event.getY(); // Enregistre la position initiale du toucher
-              return true;
-            case MotionEvent.ACTION_UP:
-              float finalY = event.getY(); // Position finale du toucher
-              if (initialY - finalY > 100) {
-                // Glissement vers le haut : image suivante
-                currentIndex = (currentIndex + 1) % imageResources.length;
-              } else if (finalY - initialY > 100) {
-                // Glissement vers le bas : image précédente
-                currentIndex = (currentIndex - 1 + imageResources.length) % imageResources.length;
-              }
-              // Mettre à jour l'image et le texte du bouton
-              backgroundImage.setImageResource(imageResources[currentIndex]);
-              slideButton.setText(String.valueOf(currentIndex));
-              return true;
-          }
-          return false;
-        }
-      });
-
-      //Flo boutton info
-      Button helpButton = findViewById(R.id.helpButton);
-      infoImage = findViewById(R.id.infoImage);
-
-      helpButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-          // Afficher l'image
-          infoImage.setVisibility(View.VISIBLE);
-
-          // Masquer l'image après 4 secondes (4000 ms)
-          handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-              infoImage.setVisibility(View.GONE);
+    slideButton.setOnTouchListener(new View.OnTouchListener() {
+      @Override
+      public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+          case MotionEvent.ACTION_DOWN:
+            initialY = event.getY(); // Enregistre la position initiale du toucher
+            return true;
+          case MotionEvent.ACTION_UP:
+            float finalY = event.getY(); // Position finale du toucher
+            if (initialY - finalY > 100) {
+              // Glissement vers le haut : image suivante
+              currentIndex = (currentIndex + 1) % imageResources.length;
+            } else if (finalY - initialY > 100) {
+              // Glissement vers le bas : image précédente
+              currentIndex = (currentIndex - 1 + imageResources.length) % imageResources.length;
             }
-          }, 4000);
+            // Mettre à jour l'image et le texte du bouton
+            backgroundImage.setImageResource(imageResources[currentIndex]);
+            slideButton.setText(String.valueOf(currentIndex));
+            return true;
         }
-      });
+        return false;
+      }
+    });
 
-      // Flo images svg zoom
-      backgroundImage = findViewById(R.id.backgroundImage);
-      backgroundImage.setImageResource(R.drawable.school_map1); // Assurez-vous que c'est un VectorDrawable
+    //Flo boutton info
+    Button helpButton = findViewById(R.id.helpButton);
+    infoImage = findViewById(R.id.infoImage);
 
-      // Initialisation du ScaleGestureDetector
-      scaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
+    helpButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        // Afficher l'image
+        infoImage.setVisibility(View.VISIBLE);
 
-      // Appliquer un OnTouchListener pour détecter le zoom
-      backgroundImage.setOnTouchListener(new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-          scaleGestureDetector.onTouchEvent(event);  // Passe les événements au ScaleGestureDetector
-          return true;
-        }
-      });
-    }
+        // Masquer l'image après 4 secondes (4000 ms)
+        handler.postDelayed(new Runnable() {
+          @Override
+          public void run() {
+            infoImage.setVisibility(View.GONE);
+          }
+        }, 4000);
+      }
+    });
+
+    // Flo images svg zoom
+    backgroundImage = findViewById(R.id.backgroundImage);
+    backgroundImage.setImageResource(R.drawable.school_map1); // Assurez-vous que c'est un VectorDrawable
+
+    // Initialisation du ScaleGestureDetector
+    scaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
+
+    // Appliquer un OnTouchListener pour détecter le zoom
+    backgroundImage.setOnTouchListener(new View.OnTouchListener() {
+      @Override
+      public boolean onTouch(View v, MotionEvent event) {
+        scaleGestureDetector.onTouchEvent(event);  // Passe les événements au ScaleGestureDetector
+        return true;
+      }
+    });
+    // ADE
+    Calendar calendar = Calendar.getInstance();
+    calendar.set(2024, Calendar.NOVEMBER, 25, 8, 30);  // 25 novembre 2023, 8h30
+    Date targetDate = calendar.getTime();
+
+    // Lancer le fetcher de calendrier
+    new CalendarFetcher(targetDate).execute();
+  }
 
   // Classe interne pour gérer les événements de zoom
-// Sert pour le ScaleListener du zoom
+  // Sert pour le ScaleListener du zoom
   private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
     @Override
     public boolean onScale(ScaleGestureDetector detector) {
