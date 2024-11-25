e2cd9b11c75a69779d3c9e3c5bf64666f20859f3
Noel Flantier <moi>
2024-11-25 18:05:41 +0100

    Details et rectifications

--- Firebase/app/src/main/java/github/yvesbenabou/firebase/ConfirmRoomButton.java
+++ Firebase/app/src/main/java/github/yvesbenabou/firebase/ConfirmRoomButton.java
@@ -11,44 +11,44 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.FirebaseDatabase;
 
 public final class ConfirmRoomButton extends FloatingActionButton implements Database_Input {
-  private final String floors = "étages";
-  private DoorButton mDoorButton;
-  public static int green;
-  public static int red;
-  private String room = "";
+    private final String floors = "étages";
+    private DoorButton mDoorButton;
+    public static int green;
+    public static int red;
+    private String room = "";
 
-  public ConfirmRoomButton(@NonNull Context context) {
-    super(context);
-  }
+    public ConfirmRoomButton(@NonNull Context context) {
+        super(context);
+    }
 
-  public ConfirmRoomButton(@NonNull Context context, @Nullable AttributeSet attrs) {
-    super(context, attrs);
-  }
+    public ConfirmRoomButton(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-  public ConfirmRoomButton(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-    super(context, attrs, defStyleAttr);
-  }
+    public ConfirmRoomButton(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 
-  public void setup(DoorButton button) {
-    this.mDoorButton = button;
-    green = getColorNormal();
-    red = getColorDisabled();
-  }
+    public void setup(DoorButton button) {
+        this.mDoorButton = button;
+        green = getColorNormal();
+        red = getColorDisabled();
+    }
 
-  public void hide() {
-    this.mDoorButton.hide();
-  }
+    public void hide() {
+        this.mDoorButton.hide();
+    }
 
-  @Override
-  public void take_room() {
-    FirebaseDatabase.getInstance().getReference().child(floors).child(String.valueOf(room.charAt(1))).child(room).setValue(Status.RESERVED.ordinal());
-  }
+    @Override
+    public void take_room() {
+        FirebaseDatabase.getInstance().getReference().child(floors).child(String.valueOf(room.charAt(1))).child(room).setValue(Status.RESERVED.ordinal());
+    }
 
-  public String getRoom() {
-    return room;
-  }
+    public String getRoom() {
+        return room;
+    }
 
-  public void setRoom(String room) {
-    this.room = room;
-  }
+    public void setRoom(String room) {
+        this.room = room;
+    }
 }
--- Firebase/app/src/main/java/github/yvesbenabou/firebase/libs/CalendarFetcher.java
+++ Firebase/app/src/main/java/github/yvesbenabou/firebase/libs/CalendarFetcher.java
@@ -69,7 +69,6 @@ public class CalendarFetcher extends AsyncTask<Void, Void, Void> {
             if (ical != null) {
                 List<VEvent> events = ical.getEvents();
 
-                MainActivity.Liste_Salles rooms = new MainActivity.Liste_Salles();
 
                 // Parcourir les événements et vérifier si la date cible est entre la date de début et de fin
                 for (VEvent event : events) {
