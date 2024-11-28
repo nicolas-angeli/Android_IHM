229b264d0d4808769c15e5a2820c6f63b5a8178f
EL5E31 <florian.bigotteau@edu.esiee.fr>
2024-11-28 15:34:37 +0100

    Création de tous les boutons java + xml

--- Firebase/app/src/main/java/github/yvesbenabou/firebase/CalendarFetcher.java
+++ Firebase/app/src/main/java/github/yvesbenabou/firebase/CalendarFetcher.java
@@ -96,7 +96,7 @@ public class CalendarFetcher {
         }
     }
 
-    public static void show() {
+    public static void show_1() {
         CalendarFetcher.rooms.getSalle("1101").show();
         CalendarFetcher.rooms.getSalle("1103").show();
         CalendarFetcher.rooms.getSalle("1105").show();
@@ -120,9 +120,131 @@ public class CalendarFetcher {
         CalendarFetcher.rooms.getSalle("5103").show();
         CalendarFetcher.rooms.getSalle("5105").show();
         CalendarFetcher.rooms.getSalle("5107").show();
+
+        CalendarFetcher.rooms.getSalle("0110").show();
+        CalendarFetcher.rooms.getSalle("0160").show();
+        CalendarFetcher.rooms.getSalle("0115").show();
+        CalendarFetcher.rooms.getSalle("0112").show();
+        CalendarFetcher.rooms.getSalle("0113").show();
+        CalendarFetcher.rooms.getSalle("0114").show();
+
+        CalendarFetcher.rooms.getSalle("0160").show();
+        CalendarFetcher.rooms.getSalle("0161").show();
+        CalendarFetcher.rooms.getSalle("0162").show();
+        CalendarFetcher.rooms.getSalle("0163").show();
+        CalendarFetcher.rooms.getSalle("0164").show();
+        CalendarFetcher.rooms.getSalle("0165").show();
+
     }
-    
-    public static void hide() {
+
+    public static void show_2() {
+        CalendarFetcher.rooms.getSalle("1201").show();
+        CalendarFetcher.rooms.getSalle("1203").show();
+        CalendarFetcher.rooms.getSalle("1205").show();
+        CalendarFetcher.rooms.getSalle("1207").show();
+
+        CalendarFetcher.rooms.getSalle("2201").show();
+        CalendarFetcher.rooms.getSalle("2203").show();
+        CalendarFetcher.rooms.getSalle("2205").show();
+        CalendarFetcher.rooms.getSalle("2207").show();
+
+        CalendarFetcher.rooms.getSalle("3201").show();
+        CalendarFetcher.rooms.getSalle("3203").show();
+        CalendarFetcher.rooms.getSalle("3205").show();
+        CalendarFetcher.rooms.getSalle("3207").show();
+        CalendarFetcher.rooms.getSalle("3209").show();
+
+        CalendarFetcher.rooms.getSalle("4205").show();
+        CalendarFetcher.rooms.getSalle("4209").show();
+        CalendarFetcher.rooms.getSalle("5201").show();
+        CalendarFetcher.rooms.getSalle("5203").show();
+        CalendarFetcher.rooms.getSalle("5205").show();
+        CalendarFetcher.rooms.getSalle("5207").show();
+
+        CalendarFetcher.rooms.getSalle("0210").show();
+        CalendarFetcher.rooms.getSalle("0260").show();
+
+    }
+
+    public static void show_3() {
+        CalendarFetcher.rooms.getSalle("1301").show();
+        CalendarFetcher.rooms.getSalle("1303").show();
+        CalendarFetcher.rooms.getSalle("1305").show();
+        CalendarFetcher.rooms.getSalle("1307").show();
+
+        CalendarFetcher.rooms.getSalle("2301").show();
+        CalendarFetcher.rooms.getSalle("2303").show();
+        CalendarFetcher.rooms.getSalle("2305").show();
+        CalendarFetcher.rooms.getSalle("2307").show();
+
+        CalendarFetcher.rooms.getSalle("3301").show();
+        CalendarFetcher.rooms.getSalle("3303").show();
+        CalendarFetcher.rooms.getSalle("3305").show();
+        CalendarFetcher.rooms.getSalle("3307").show();
+        CalendarFetcher.rooms.getSalle("3309").show();
+
+        CalendarFetcher.rooms.getSalle("4305").show();
+        CalendarFetcher.rooms.getSalle("4309").show();
+        CalendarFetcher.rooms.getSalle("5301").show();
+        CalendarFetcher.rooms.getSalle("5303").show();
+        CalendarFetcher.rooms.getSalle("5305").show();
+        CalendarFetcher.rooms.getSalle("5307").show();
+    }
+
+    public static void show_4() {
+        CalendarFetcher.rooms.getSalle("1401").show();
+        CalendarFetcher.rooms.getSalle("1403").show();
+        CalendarFetcher.rooms.getSalle("1405").show();
+        CalendarFetcher.rooms.getSalle("1407").show();
+
+        CalendarFetcher.rooms.getSalle("2401").show();
+        CalendarFetcher.rooms.getSalle("2403").show();
+        CalendarFetcher.rooms.getSalle("2405").show();
+        CalendarFetcher.rooms.getSalle("2407").show();
+
+        CalendarFetcher.rooms.getSalle("3401").show();
+        CalendarFetcher.rooms.getSalle("3403").show();
+        CalendarFetcher.rooms.getSalle("3405").show();
+        CalendarFetcher.rooms.getSalle("3407").show();
+        CalendarFetcher.rooms.getSalle("3409").show();
+
+        CalendarFetcher.rooms.getSalle("4405").show();
+        CalendarFetcher.rooms.getSalle("4409").show();
+
+        CalendarFetcher.rooms.getSalle("5401").show();
+        CalendarFetcher.rooms.getSalle("5403").show();
+        CalendarFetcher.rooms.getSalle("5405").show();
+        CalendarFetcher.rooms.getSalle("5407").show();
+    }
+
+    public static void show_0() {
+        CalendarFetcher.rooms.getSalle("1001").show();
+        CalendarFetcher.rooms.getSalle("1003").show();
+        CalendarFetcher.rooms.getSalle("1005").show();
+        CalendarFetcher.rooms.getSalle("1007").show();
+
+        CalendarFetcher.rooms.getSalle("2001").show();
+        CalendarFetcher.rooms.getSalle("2003").show();
+        CalendarFetcher.rooms.getSalle("2005").show();
+        CalendarFetcher.rooms.getSalle("2007").show();
+
+        CalendarFetcher.rooms.getSalle("3001").show();
+        CalendarFetcher.rooms.getSalle("3003").show();
+        CalendarFetcher.rooms.getSalle("3005").show();
+        CalendarFetcher.rooms.getSalle("3007").show();
+        CalendarFetcher.rooms.getSalle("3009").show();
+
+        CalendarFetcher.rooms.getSalle("4005").show();
+        CalendarFetcher.rooms.getSalle("4009").show();
+
+        CalendarFetcher.rooms.getSalle("5001").show();
+        CalendarFetcher.rooms.getSalle("5003").show();
+        CalendarFetcher.rooms.getSalle("5005").show();
+        CalendarFetcher.rooms.getSalle("5007").show();
+    }
+
+
+    public static void hide_1() {
         CalendarFetcher.rooms.getSalle("1101").hide();
         CalendarFetcher.rooms.getSalle("1103").hide();
         CalendarFetcher.rooms.getSalle("1105").hide();
@@ -146,5 +268,124 @@ public class CalendarFetcher {
         CalendarFetcher.rooms.getSalle("5103").hide();
         CalendarFetcher.rooms.getSalle("5105").hide();
         CalendarFetcher.rooms.getSalle("5107").hide();
+
+        CalendarFetcher.rooms.getSalle("0110").hide();
+        CalendarFetcher.rooms.getSalle("0160").hide();
+        CalendarFetcher.rooms.getSalle("0115").hide();
+        CalendarFetcher.rooms.getSalle("0112").hide();
+        CalendarFetcher.rooms.getSalle("0113").hide();
+        CalendarFetcher.rooms.getSalle("0114").hide();
+
+        CalendarFetcher.rooms.getSalle("0160").hide();
+        CalendarFetcher.rooms.getSalle("0161").hide();
+        CalendarFetcher.rooms.getSalle("0162").hide();
+        CalendarFetcher.rooms.getSalle("0163").hide();
+        CalendarFetcher.rooms.getSalle("0164").hide();
+        CalendarFetcher.rooms.getSalle("0165").hide();
+    }
+
+    public static void hide_2() {
+        CalendarFetcher.rooms.getSalle("1201").hide();
+        CalendarFetcher.rooms.getSalle("1203").hide();
+        CalendarFetcher.rooms.getSalle("1205").hide();
+        CalendarFetcher.rooms.getSalle("1207").hide();
+
+        CalendarFetcher.rooms.getSalle("2201").hide();
+        CalendarFetcher.rooms.getSalle("2203").hide();
+        CalendarFetcher.rooms.getSalle("2205").hide();
+        CalendarFetcher.rooms.getSalle("2207").hide();
+
+        CalendarFetcher.rooms.getSalle("3201").hide();
+        CalendarFetcher.rooms.getSalle("3203").hide();
+        CalendarFetcher.rooms.getSalle("3205").hide();
+        CalendarFetcher.rooms.getSalle("3207").hide();
+        CalendarFetcher.rooms.getSalle("3209").hide();
+
+        CalendarFetcher.rooms.getSalle("4205").hide();
+        CalendarFetcher.rooms.getSalle("4209").hide();
+        CalendarFetcher.rooms.getSalle("5201").hide();
+        CalendarFetcher.rooms.getSalle("5203").hide();
+        CalendarFetcher.rooms.getSalle("5205").hide();
+        CalendarFetcher.rooms.getSalle("5207").hide();
+
+        CalendarFetcher.rooms.getSalle("0210").hide();
+        CalendarFetcher.rooms.getSalle("0260").hide();
+    }
+
+    public static void hide_3() {
+        CalendarFetcher.rooms.getSalle("1301").hide();
+        CalendarFetcher.rooms.getSalle("1303").hide();
+        CalendarFetcher.rooms.getSalle("1305").hide();
+        CalendarFetcher.rooms.getSalle("1307").hide();
+
+        CalendarFetcher.rooms.getSalle("2301").hide();
+        CalendarFetcher.rooms.getSalle("2303").hide();
+        CalendarFetcher.rooms.getSalle("2305").hide();
+        CalendarFetcher.rooms.getSalle("2307").hide();
+
+        CalendarFetcher.rooms.getSalle("3301").hide();
+        CalendarFetcher.rooms.getSalle("3303").hide();
+        CalendarFetcher.rooms.getSalle("3305").hide();
+        CalendarFetcher.rooms.getSalle("3307").hide();
+        CalendarFetcher.rooms.getSalle("3309").hide();
+
+        CalendarFetcher.rooms.getSalle("4305").hide();
+        CalendarFetcher.rooms.getSalle("4309").hide();
+        CalendarFetcher.rooms.getSalle("5301").hide();
+        CalendarFetcher.rooms.getSalle("5303").hide();
+        CalendarFetcher.rooms.getSalle("5305").hide();
+        CalendarFetcher.rooms.getSalle("5307").hide();
+    }
+
+    public static void hide_4() {
+        CalendarFetcher.rooms.getSalle("1401").hide();
+        CalendarFetcher.rooms.getSalle("1403").hide();
+        CalendarFetcher.rooms.getSalle("1405").hide();
+        CalendarFetcher.rooms.getSalle("1407").hide();
+
+        CalendarFetcher.rooms.getSalle("2401").hide();
+        CalendarFetcher.rooms.getSalle("2403").hide();
+        CalendarFetcher.rooms.getSalle("2405").hide();
+        CalendarFetcher.rooms.getSalle("2407").hide();
+
+        CalendarFetcher.rooms.getSalle("3401").hide();
+        CalendarFetcher.rooms.getSalle("3403").hide();
+        CalendarFetcher.rooms.getSalle("3405").hide();
+        CalendarFetcher.rooms.getSalle("3407").hide();
+        CalendarFetcher.rooms.getSalle("3409").hide();
+
+        CalendarFetcher.rooms.getSalle("4405").hide();
+        CalendarFetcher.rooms.getSalle("4409").hide();
+
+        CalendarFetcher.rooms.getSalle("5401").hide();
+        CalendarFetcher.rooms.getSalle("5403").hide();
+        CalendarFetcher.rooms.getSalle("5405").hide();
+        CalendarFetcher.rooms.getSalle("5407").hide();
+    }
+
+    public static void hide_0() {
+        CalendarFetcher.rooms.getSalle("1001").hide();
+        CalendarFetcher.rooms.getSalle("1003").hide();
+        CalendarFetcher.rooms.getSalle("1005").hide();
+        CalendarFetcher.rooms.getSalle("1007").hide();
+
+        CalendarFetcher.rooms.getSalle("2001").hide();
+        CalendarFetcher.rooms.getSalle("2003").hide();
+        CalendarFetcher.rooms.getSalle("2005").hide();
+        CalendarFetcher.rooms.getSalle("2007").hide();
+
+        CalendarFetcher.rooms.getSalle("3001").hide();
+        CalendarFetcher.rooms.getSalle("3003").hide();
+        CalendarFetcher.rooms.getSalle("3005").hide();
+        CalendarFetcher.rooms.getSalle("3007").hide();
+        CalendarFetcher.rooms.getSalle("3009").hide();
+
+        CalendarFetcher.rooms.getSalle("4005").hide();
+        CalendarFetcher.rooms.getSalle("4009").hide();
+
+        CalendarFetcher.rooms.getSalle("5001").hide();
+        CalendarFetcher.rooms.getSalle("5003").hide();
+        CalendarFetcher.rooms.getSalle("5005").hide();
+        CalendarFetcher.rooms.getSalle("5007").hide();
     }
 }
