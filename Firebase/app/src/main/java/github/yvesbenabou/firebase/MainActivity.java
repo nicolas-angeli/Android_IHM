b21f21c236d5ae4c9f1031c0d338536f9582dc23
Noel Flantier <moi>
2024-12-02 21:33:21 +0100

    refresh ade lors de l'ouverture

--- Firebase/app/src/main/java/github/yvesbenabou/firebase/MainActivity.java
+++ Firebase/app/src/main/java/github/yvesbenabou/firebase/MainActivity.java
@@ -78,6 +78,7 @@ public class MainActivity extends AppCompatActivity{
 
 
     @Override protected void onCreate(Bundle savedInstanceState) {
+        ADE_refresh();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
